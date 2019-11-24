2a0075172fa1637707078da64241fe2b7ab95a3f
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2019-11-24 21:31:10 +0100

    Front page HTML now generated from a template

--- app/functional_tests.py
+++ app/functional_tests.py
@@ -1,5 +1,9 @@
-from selenium import webdriver
+import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -38,7 +42,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_recs_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == 'Revolutionary Girl Utena' for row in rows)
+            any(row.text == 'Revolutionary Girl Utena' for row in rows),
+            "Recommendation did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She

1fcec9013ef45643e6ea7ad0a1869e8037852c00
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2019-12-01 10:47:43 +0100

    FT uses TestDatabase and selenium docker. FT broke

--- app/functional_tests/tests.py
+++ app/functional_tests/tests.py
@@ -1,32 +1,56 @@
+import time
+import socket
+
+from django.test import LiveServerTestCase
+from django.test import tag, override_settings
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-import unittest
-import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-class NewVisitorTest(unittest.TestCase):
+@tag('selenium')
+@override_settings(ALLOWED_HOSTS=['*'])
+class SeleniumLiveServerTestCase(LiveServerTestCase):
+    """
+    Provides base test class which connects to the Docker
+    container running selenium.
+    """
+    host = '0.0.0.0'
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.host = socket.gethostbyname(socket.gethostname())
+        cls.selenium = webdriver.Remote(
+            command_executor='http://selenium:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.FIREFOX
+        )
+        cls.selenium.implicitly_wait(5)
 
-    def tearDown(self):
-        self.browser.quit()
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+
+
+class NewVisitorTest(SeleniumLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get("http://localhost:8000")
+        self.selenium.get(self.live_server_url)
 
         # She notices the page title and header mention punpun 
-        self.assertIn('punpun.me', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('punpun.me', self.selenium.title)
+        header_text = self.selenium.find_element_by_tag_name('h1').text
         self.assertIn('punpun.me', header_text)
-        subheader_text = self.browser.find_element_by_tag_name('h2').text
+        subheader_text = self.selenium.find_element_by_tag_name('h2').text
         self.assertIn('a simple anime recommender system', subheader_text)
 
         # She is invited to enter her mal username straight away
-        inputbox = self.browser.find_element_by_id('id_enter_username')
+        inputbox = self.selenium.find_element_by_id('id_enter_username')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter your MyAnimeList username'
@@ -55,7 +79,3 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # Satisfied he goes back to sleep.
-
-
-if __name__ == '__main__':
-    unittest.main()

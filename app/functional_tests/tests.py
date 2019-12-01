92bf73651147837461d1ea9e92e6a428463c5fac
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2019-12-01 11:16:44 +0100

    Smarter wait in FT

--- app/functional_tests/tests.py
+++ app/functional_tests/tests.py
@@ -7,6 +7,10 @@ from django.test import tag, override_settings
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import WebDriverException
+
+
+MAX_WAIT = 20
 
 
 @tag('selenium')
@@ -37,37 +41,47 @@ class SeleniumLiveServerTestCase(LiveServerTestCase):
 
 class NewVisitorTest(SeleniumLiveServerTestCase):
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
+    def wait_for_welcome_message_after_entering_username(self, username):
+        t_start = time.time()
+        while True:
+            try:
+                welcome_msg = self.selenium.find_element_by_id('id_welcome_msg').text
+                self.assertIn(username, welcome_msg)
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - t_start > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+
+    def test_can_enter_a_username_and_retrieve_recommendations(self):
+        # Manuel has heard about a cool new anime recommender system.
+        # He goes to check out its homepage.
         self.selenium.get(self.live_server_url)
 
-        # She notices the page title and header mention punpun 
+        # He notices the page title and header mention 'punpun.me' and
+        # anime recommender systems.
         self.assertIn('punpun.me', self.selenium.title)
         header_text = self.selenium.find_element_by_tag_name('h1').text
         self.assertIn('punpun.me', header_text)
         subheader_text = self.selenium.find_element_by_tag_name('h2').text
         self.assertIn('a simple anime recommender system', subheader_text)
 
-        # She is invited to enter her mal username straight away
+        # He is invited to enter his MyAnimeList username straight away.
         inputbox = self.selenium.find_element_by_id('id_enter_username')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter your MyAnimeList username'
         )
 
-        # She enters 'Manuel' right away
+        # He enters his name: 'Manuel' and hits Enter. 
         inputbox.send_keys('Manuel')
-
-        # When she hits enter, the page updates 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
 
-        # A welcome message is displayed.
-        welcome_msg = self.selenium.find_element_by_id('id_welcome_msg').text
-        self.assertIn('Manuel', welcome_msg)
+        # The page loads for some time and a welcome message is displayed.
+        self.wait_for_welcome_message_after_entering_username('Manuel')
 
-        # A list of one hundred recommendations is displayed. 
+        # Also, a list of one hundred recommendations is displayed. 
         table = self.selenium.find_element_by_id('id_recs_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertEqual(len(rows), 100)
